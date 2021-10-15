99a9f901da688a7f3324b30ef7b2705b372bc774
Adam Ray <aray58@gmail.com>
2021-10-15 16:35:18 -0500

    Part 1 Complete

--- js-basics-1/sldc.js
+++ js-basics-1/sldc.js
@@ -4,16 +4,27 @@
 /*
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
-
+// Planning
 // Analysis of Requirements
 // Design
 // Implementation
-// Maintain
-// Planning
 // Testing & Integration
-
+// Maintain
 
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
 */
+let Planning = "1. Planning is the stage where developers plan for the project"
+let analysisOfRequirements = "2. Analysis of Requirements is the stage where developers gather details, define system requirements, perform research, and evaluate alternatives "
+let Design = "3. Design is the stage where developers will outline details for the application"
+let Implementation = "4. Implementation is the stage where the design is coded"
+let testingAndIntegration = "5. Testing & Integration is the stage where the all modules are integrated together and it is tested for bugs and client expectations "
+let Maintain = "6. Maintain is the stage where the team will maintain the product and fix issues as they arise"
+
+// console.log(Planning);
+// console.log(analysisOfRequirements);
+// console.log(Design);
+// console.log(Implementation);
+// console.log(testingAndIntegration);
+// console.log(Maintain);
