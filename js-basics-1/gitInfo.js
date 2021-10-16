6b72e808243bd7a40b55ee06028df07f73912774
Adam Ray <aray58@gmail.com>
2021-10-15 19:35:13 -0500

    gitinfo.js complete

--- js-basics-1/gitInfo.js
+++ js-basics-1/gitInfo.js
@@ -10,6 +10,7 @@
 */
 
 //CODE HERE
+let gitDefinition = "Git is a version control system"
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -18,6 +19,8 @@
 */
 
 //CODE HERE
+
+let gitHubDefinition = "GitHub is a website and cloud based service which developers can use to store, manage, track, and control code"
 
 //////////////////PROBLEMS 3 - 8////////////////////
 /*
@@ -34,6 +37,8 @@
 */
 
 //CODE HERE
+// let object = {description: "",code: ""};
+let init = {description: "initialize a local repository in this folder",code: "git init"};
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -43,6 +48,8 @@
 
 //CODE HERE
 
+let clone = {description: "copy a repository from Github and put it on my computer",code: "git clone REPO_URL"};
+
 //////////////////PROBLEM 5////////////////////
 /*
     Create an object called 'status' with 'description' and 'code' properties 
@@ -50,6 +57,8 @@
 */
 
 //CODE HERE
+
+let status = {description: "tell me about this repository",code: "git status"};
 
 //////////////////PROBLEM 6////////////////////
 /*
@@ -59,6 +68,8 @@
 
 //CODE HERE
 
+let add = {description: "add files to be tracked, use add . for all in folder, use add example.js for a specific file",code: "git add"};
+
 //////////////////PROBLEM 7////////////////////
 /*
     Create an object called 'commit' with 'description' and 'code' properties
@@ -67,6 +78,7 @@
 
 //CODE HERE
 
+let commit = {description: "create a snapshot version of the files im tracking and add commit message",code: `git commit -m"" `};
 
 //////////////////PROBLEM 8////////////////////
 /*
@@ -75,3 +87,5 @@
 */
 
 //CODE HERE
+
+let push = {description: "put my local commits on Github",code: "git push"};
